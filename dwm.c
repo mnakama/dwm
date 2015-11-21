b45102cadcd558b5af78848fa0a129fc9efaeac7
Matt Boswell <mordervomubel+gitlab@lockmail.us>
2015-11-20 22:07:33 -0500

    add const for move/resize update limit

--- dwm.c
+++ dwm.c
@@ -1342,7 +1342,7 @@ movemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= minupdateinterval)
 				continue;
 			lasttime = ev.xmotion.time;
 
@@ -1571,7 +1571,7 @@ resizemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= minupdateinterval)
 				continue;
 			lasttime = ev.xmotion.time;
 
